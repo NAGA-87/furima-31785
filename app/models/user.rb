552861bfbbd7b099d5ca3e2fea1629261e717325
NAGA-87 <naga.2000.chr5@gmail.com>
2020-11-17 14:39:19 +0900

    ユーザー管理機能_修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  with_options presence: true do
+    validates :last_name
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  end
+
+  with_options presence: true do
+    validates :last_name_kana
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  end
   validates :user_birthday, presence: true
   # VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[\w-]+\z/i.freeze
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[\w-]+\z/i, message: 'Include both letters and numbers' }
