c2e6766db3e3abe348a496dfbc7fe820f86e99a7
NAGA-87 <naga.2000.chr5@gmail.com>
2020-11-19 16:56:18 +0900

    テスト実施後の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,11 @@
 class Item < ApplicationRecord
-
   validates :image, presence: true
   validates :title,               presence: true
   validates :explain,             presence: true
   validates :price,               presence: true, numericality: { greater_than: 299, less_than: 10_000_000, message: 'Out of setting range' }
   validates :price,               presence: true, numericality: { only_integer: true, message: 'Half-width number' }
   # アクティブハッシュの選択が「---」の時は保存されない
-  validates :category_id, :condition_id, :fee_id, :prefecture_id, :days_to_delivery_id, numericality: { other_than: 1, message: 'status Select'}
+  validates :category_id, :condition_id, :fee_id, :prefecture_id, :days_to_delivery_id, numericality: { other_than: 1, message: 'status Select' }
 
   # アソシエーション
   belongs_to :user
@@ -19,5 +18,4 @@ class Item < ApplicationRecord
   belongs_to :fee
   belongs_to :prefecture
   belongs_to :days_to_delivery
-
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-
   describe '商品出品機能' do
     before do
       @item = FactoryBot.build(:item)
@@ -25,27 +24,27 @@ RSpec.describe Item, type: :model do
     it 'カテゴリーの情報が空だと登録できない' do
       @item.category_id = '1'
       @item.valid?
-      expect(@item.errors.full_messages).to include("Category status Select")
+      expect(@item.errors.full_messages).to include('Category status Select')
     end
     it '商品の状態が空だと登録できない' do
       @item.condition_id = '1'
       @item.valid?
-      expect(@item.errors.full_messages).to include("Condition status Select")
+      expect(@item.errors.full_messages).to include('Condition status Select')
     end
     it '配送料の負担が空だと登録できない' do
       @item.fee_id = '1'
       @item.valid?
-      expect(@item.errors.full_messages).to include("Fee status Select")
+      expect(@item.errors.full_messages).to include('Fee status Select')
     end
     it '発送元の地域が空だと登録できない' do
       @item.prefecture_id = '1'
       @item.valid?
-      expect(@item.errors.full_messages).to include("Prefecture status Select")
+      expect(@item.errors.full_messages).to include('Prefecture status Select')
     end
     it '発送までの日数が空だと登録できない' do
       @item.days_to_delivery_id = '1'
       @item.valid?
-      expect(@item.errors.full_messages).to include("Days to delivery status Select")
+      expect(@item.errors.full_messages).to include('Days to delivery status Select')
     end
     it '価格が空だと登録できない' do
       @item.price = nil
@@ -55,19 +54,19 @@ RSpec.describe Item, type: :model do
     it '価格が¥9999999を超えると登録できない' do
       @item.price = '10000000'
       @item.valid?
-      expect(@item.errors.full_messages).to include("Price Out of setting range")
+      expect(@item.errors.full_messages).to include('Price Out of setting range')
     end
     it '価格が¥300より低いと登録できない' do
       @item.price = '299'
       @item.valid?
-      expect(@item.errors.full_messages).to include("Price Out of setting range")
+      expect(@item.errors.full_messages).to include('Price Out of setting range')
     end
     it '価格は半角数字以外登録できない' do
       @item.price = '１００００'
       @item.valid?
-      expect(@item.errors.full_messages).to include("Price Half-width number")
+      expect(@item.errors.full_messages).to include('Price Half-width number')
     end
-    it "入力必須項目が存在すれば登録できること" do
+    it '入力必須項目が存在すれば登録できること' do
       expect(@item).to be_valid
     end
   end
